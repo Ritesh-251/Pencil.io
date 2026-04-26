065b3b3710ba5c26a0bf319efd487098d7645516
Ritesh-251 <hareradheharekrishna23@gmail.com>
2026-04-26 23:09:56 +0530

    chore(http): update dependencies and export shared redis client

--- apps/http-backend/src/infra/redis.ts
+++ apps/http-backend/src/infra/redis.ts
@@ -1,6 +1,16 @@
 import { RedisPubSub } from "@repo/redis";
+import Redis from "ioredis";
 
 export const pubsub = new RedisPubSub("http-backend");
+
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error("REDIS_URL is not defined");
+}
+
+export const redisClient = new Redis(redisUrl, {
+  retryStrategy: (times: number) => Math.min(times * 200, 5000),
+});
 
 export async function initRedis() {
   await pubsub.connect();
